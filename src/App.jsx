29589e5469889a0d86709dfa262b3a0de0ca032d
Raymond <raymondurrutia2@yahoo.com>
2022-08-17 11:50:57 -0400

    Added functionality to move modified notes to top of list

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,22 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        // Put the most recently-modified note at the top
+        setNotes(oldNotes => {
+            // Create a new empty array
+            const newArray = []
+            // Loop over the original array
+            for (let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                  newArray.unshift({ ...oldNote, body: text })
+                } else {
+                  newArray.push(oldNote)
+                }
+            }
+
+            return newArray
+        })
     }
     
     function findCurrentNote() {
